6163f14faf1d37471c02dbe11ec7f729e865edb1
ozi <ozisadiqkasai@gmail.com>
2025-02-24 12:46:29 +0100

    Gender Section done

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         alt="clothes"
         className="w-full h-{400px] md:h-[600px] lg:h-[750px] object-cover"
       />
-      <div className="absolute inset-0 bg-black/5 flex items-center justify-center">
+      <div className="absolute inset-0 bg-black/50 flex items-center justify-center">
 
         <div className="text-center text-white p-6 ">
           <h1 className="text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4">
