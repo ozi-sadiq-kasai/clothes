24c52671eab5477857b88b2d9b2ea8538f19955e
ozi <ozisadiqkasai@gmail.com>
2025-02-28 22:06:03 +0100

    cart styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,18 +7,22 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import CollectionPage from "./pages/CollectionPage";
+import ProductDetails from "./components/Products/ProductDetails";
+import Checkout from "./components/Cart/Checkout";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Toaster position="top-right"/>
         <Routes>
-          <Route path="/" element={<UserLayout />}>
+          < Route path="/" element={<UserLayout />}>
             <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
             <Route path="profile" element={<Profile />} />
             <Route path="collections/:collection" element={<CollectionPage />} />
+            <Route path='product/:id' element={<ProductDetails/>}/>
+            <Route path='/checkout' element={<Checkout/>}/>
           </Route>
           <Route> {/* AdminLayout */}</Route>
         </Routes>
