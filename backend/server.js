eb9e0372c9aa7e6623a8b753938bce28dadbc8f2
ozi <ozisadiqkasai@gmail.com>
2025-03-10 19:41:34 +0100

    mongo db connected

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require('express')
 const cors = require('cors')
+const connectDB = require('./config/db')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-const PORT = 4000
+const PORT = process.env.PORT || 3000
 
-app.get('/',(req,res)=>{
+//Connect database
+connectDB()
+
+app.get('/', (req, res) => {
     res.send('Hello')
 })
 
-app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)})
+app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`) })
