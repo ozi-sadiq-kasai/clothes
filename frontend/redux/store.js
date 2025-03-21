3f7b74f8f1bac373e32a28ccb3bc023cc51281da
ozi <ozisadiqkasai@gmail.com>
2025-03-21 23:48:47 +0100

    admin reducer

--- frontend/redux/store.js
+++ frontend/redux/store.js
@@ -2,6 +2,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import productReducer from './slices/productsSlice';
 import cartReducer from './slices/cartSlice';
+import checkoutReducer from './slices/checkoutSlice';
+import orderSliceReducer from './slices/orderSlice';
+import adminReducer from './slices/adminSlice';
 
 
 const store = configureStore({
@@ -9,6 +12,9 @@ const store = configureStore({
         auth: authReducer,
         products: productReducer,
         cart: cartReducer,
+        checkout: checkoutReducer,
+        order: orderSliceReducer,
+        admin: adminReducer
     },
 });
 
